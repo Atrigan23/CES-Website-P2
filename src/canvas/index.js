1e57168fb7dae49e5a4637afd1d9892eddad2a86
Atrigan23 <pethonir@gmail.com>
2024-06-04 07:31:29 +0200

    Updates

--- src/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import CESCanvas from "./CESLogo";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, CESCanvas, StarsCanvas };
